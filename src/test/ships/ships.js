500b890c1356759cb15102ff1cb27a38a4533e20
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-08-21 16:00:31 +0800

    feat: Create ship factory function that returns the following
    feats: hit() increase ship hits;
        isSunk() change ship sunk status;
        getShipStatus() get hits and sunk status of a ship

---
+++ src/test/ships/ships.js
@@ -0,0 +1,32 @@
+const Ships = () => {
+  const ships = {
+    carrier: { length: 5, hits: 0, sunk: false },
+    battleship: { length: 4, hits: 0, sunk: false },
+    submarine: { length: 3, hits: 0, sunk: false },
+    cruise: { length: 3, hits: 0, sunk: false },
+    destroyer: { length: 2, hits: 0, sunk: false },
+  };
+
+  const hit = (ship) => {
+    ships[ship].hits += 1;
+  };
+
+  const isSunk = (ship) => {
+    ships[ship].sunk = ships[ship].length === ships[ship].hits;
+  };
+
+  const getShipStatus = (ship) => {
+    const { hits } = ships[ship];
+    const { sunk } = ships[ship];
+
+    return { hits, sunk };
+  };
+
+  return {
+    getShipStatus,
+    hit,
+    isSunk,
+  };
+};
+
+export default Ships;
