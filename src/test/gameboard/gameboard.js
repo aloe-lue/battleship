692cc195ba897e6a5999d6d3b26ead3a8eaf53de
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-08-27 15:27:02 +0800

    feat: refactor code -> rename parameters and properties
    use a keyword parameter
    increaseNum prefer literal number it covers 10

--- src/test/gameboard/gameboard.js
+++ src/test/gameboard/gameboard.js
@@ -1,50 +1,51 @@
+import Ships from '../ships/ships';
+
 const GAMEBOARD = () => {
-  const increaseNum = (coordinates, shipLength) => {
+  const ships = Ships();
+
+  // prefer keyword parameters
+  const increaseNum = ({ coordinates, shipLength }) => {
     const shipLen = shipLength;
     const [x, y] = coordinates;
     const result = [[x, y]];
 
-    if (x + shipLen > 10) {
-      return null;
-    }
-
     let asciiOfX = String(x).charCodeAt(0);
     for (let i = 1; i < shipLen; i += 1) {
       result[i] = [Number(String.fromCharCode((asciiOfX += 1))), y];
+
+      // ascii for 10 is 49 48 doesn't work on String.fromCharCodeAt(49, 48);
+      if (asciiOfX > 57) {
+        result[i] = [10, y];
+      }
     }
 
     return result;
   };
 
-  const chooseLetter = (coordinates, shipLength) => {
+  const chooseLetter = ({ coordinates, shipLength }) => {
     const shipLen = shipLength;
     const [x, y] = coordinates;
-    let result = [[x, y]];
+    const result = [[x, y]];
     let asciiOfY = y.charCodeAt(0);
 
     for (let i = 1; i < shipLen; i += 1) {
       result[i] = [x, String.fromCharCode((asciiOfY += 1))];
-
-      // ! I feel like there is something wrong here
-      if (asciiOfY > 74 || asciiOfY > 106) {
-        result = null;
-        return result;
-      }
     }
     return result;
   };
 
   const shipPlaces = [];
 
-  const getShipCoordinates = (
+  const getShipCoordinates = ({
     pairOfCoordinates,
     ship,
     axis,
     shipPairOfCoordinates,
-  ) => {
+  }) => {
     const coordinates = pairOfCoordinates;
     const shipName = ship;
     const axe = axis;
+    const shipPlace = shipPairOfCoordinates;
     const shipsLength = {
       carrier: 5,
       battleship: 4,
@@ -57,49 +58,56 @@ const GAMEBOARD = () => {
     if (axe === 'v') {
       const vertical = {
         ship: shipName,
-        coordinates: increaseNum(coordinates, shipLength),
+        coordinates: increaseNum({ coordinates, shipLength }),
       };
-
-      shipPairOfCoordinates.push(vertical);
+      shipPlace.push(vertical);
 
       return vertical;
     }
+
     const horizontal = {
       ship: shipName,
-      coordinates: chooseLetter(coordinates, shipLength),
+      coordinates: chooseLetter({ coordinates, shipLength }),
     };
+    shipPlace.push(horizontal);
 
-    shipPairOfCoordinates.push(horizontal);
     return horizontal;
   };
 
-  const handleOverlapCoordinates = () => {};
-
   const missedShots = [];
 
-  const receiveAttack = (coordinate, shipPairOfCoordinates) => {
+  const receiveAttack = ({ coordinate, shipPairOfCoordinates, missedShot }) => {
     const [x, y] = coordinate;
     const shipCoordinates = shipPairOfCoordinates;
     let isMissed = true;
     let shipName = '';
 
+    const coordinatesMaps = new Map();
     while (shipCoordinates.length !== 0) {
       const { ship, coordinates } = shipCoordinates.at(0);
 
       for (let i = 0; i < coordinates.length; i += 1) {
         const [a, b] = coordinates[i];
 
-        if (x === a && y === b) {
-          isMissed = false;
-          shipName = ship;
-        }
+        coordinatesMaps.set(`${a}-${b}`, ship);
       }
       shipCoordinates.shift();
     }
-    return isMissed ? [x, y] : shipName;
+
+    if (coordinatesMaps.has(`${x}-${y}`)) {
+      isMissed = false;
+      shipName = coordinatesMaps.get(`${x}-${y}`);
+      ships.hit(shipName);
+      return shipName;
+    }
+
+    const missed = missedShot;
+    missed.push([x, y]);
+    return [x, y];
   };
 
   return {
+    ships,
     shipPlaces,
     missedShots,
     getShipCoordinates,
