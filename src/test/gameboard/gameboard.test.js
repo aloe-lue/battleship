8dd682af68502d45c56414e67033ddf0ecb3c720
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-08-28 15:43:59 +0800

    feat: Create a duplicate test cases of enemy gameboard to see if it work

--- src/test/gameboard/gameboard.test.js
+++ src/test/gameboard/gameboard.test.js
@@ -43,7 +43,7 @@ ships.map((element) =>
 );
 
 test('place ships at specific coordinates', () => {
-  expect(gameboard.shipPlaces.get('battleship')).toEqual([
+  expect(gameboard.shipPlaces[1].coordinates).toEqual([
     [4, 'b'],
     [4, 'c'],
     [4, 'd'],
@@ -57,7 +57,7 @@ test(`receiveAttack receives a pair of coordinates,
   expect(
     gameboard.receiveAttack({
       coordinate: [4, 'b'],
-      shipPairOfCoordinates: gameboard.shipPlaces,
+      shipPlacement: gameboard.shipPlaces,
       missedShot: gameboard.missedShots,
       shipFactory: gameboard.ships,
     }),
@@ -68,11 +68,87 @@ test(`receiveAttack receives a pair of coordinates,
   expect(
     gameboard.receiveAttack({
       coordinate: [3, 'b'],
-      shipPairOfCoordinates: gameboard.shipPlaces,
+      shipPlacement: gameboard.shipPlaces,
       missedShot: gameboard.missedShots,
       shipFactory: gameboard.ships,
     }),
   ).toEqual([3, 'b']);
 
   expect(gameboard.missedShots).toEqual([[3, 'b']]);
+});
+
+const enemyShips = [
+  // had to change it like this cuz it adds lines of codes
+  {
+    ship: 'carrier',
+    coordinates: [3, 'i'],
+    axe: 'v',
+  },
+  {
+    ship: 'battleship',
+    coordinates: [3, 'c'],
+    axe: 'v',
+  },
+  {
+    ship: 'submarine',
+    coordinates: [1, 'f'],
+    axe: 'v',
+  },
+  {
+    ship: 'cruise',
+    coordinates: [8, 'd'],
+    axe: 'h',
+  },
+  {
+    ship: 'destroyer',
+    coordinates: [1, 'a'],
+    axe: 'v',
+  },
+];
+
+const enemyGameboard = Gameboard();
+
+enemyShips.map((element) =>
+  enemyGameboard.getShipCoordinates({
+    pairOfCoordinates: element.coordinates,
+    ship: element.ship,
+    axis: element.axe,
+    shipPlacement: enemyGameboard.shipPlaces,
+    shipsFactory: enemyGameboard.ships,
+  }),
+);
+
+test('place ships at specific coordinates', () => {
+  expect(enemyGameboard.shipPlaces[1].coordinates).toEqual([
+    [3, 'c'],
+    [4, 'c'],
+    [5, 'c'],
+    [6, 'c'],
+  ]);
+});
+
+test(`receiveAttack receives a pair of coordinates,
+  determines whether or not it hit a ship, 
+  if so sends the 'hit' function to the correct ship`, () => {
+  expect(
+    enemyGameboard.receiveAttack({
+      coordinate: [3, 'c'],
+      shipPlacement: enemyGameboard.shipPlaces,
+      missedShot: enemyGameboard.missedShots,
+      shipFactory: enemyGameboard.ships,
+    }),
+  ).toBe('battleship');
+
+  expect(enemyGameboard.ships.getShipStatus('battleship').hits).toBe(1);
+
+  expect(
+    enemyGameboard.receiveAttack({
+      coordinate: [3, 'b'],
+      shipPlacement: enemyGameboard.shipPlaces,
+      missedShot: enemyGameboard.missedShots,
+      shipFactory: enemyGameboard.ships,
+    }),
+  ).toEqual([3, 'b']);
+
+  expect(enemyGameboard.missedShots).toEqual([[3, 'b']]);
 });
