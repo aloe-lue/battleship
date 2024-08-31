bf892abc44fd81f0e039a59c7fccb1b7bea08a48
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-08-31 16:09:28 +0800

    feat: rewrite the code, separate the application logic from logic
    this time make a separate logic and make application logic use that
    logic to create multiple circles depending on the ships length

--- src/js/ships-hits.js
+++ src/js/ships-hits.js
@@ -6,20 +6,30 @@ const shipsLength = {
   destroyer: 2,
 };
 
-// adds circle depending on the ship
-const addCircleShipHits = ({ parent, ship }) => {
-  const shipName = ship;
-  const pElement = document.querySelector(`${parent}`);
-  pElement.setAttribute('style', 'display: flex; gap: 0.25rem;');
+const createAnArrayOfNumbers = ({ shipName }) => {
+  const shipL = shipsLength[shipName];
+  const array = [];
 
-  for (let i = 0; i < shipsLength[shipName]; i += 1) {
-    const div = document.createElement('div');
-    div.setAttribute(`data-${shipName}-hits`, `${i}`);
-    div.style = `border: 1px solid gray; border-radius: 50%; height: 1rem; width: 1rem;`;
-    pElement.appendChild(div);
+  for (let i = 0; i < shipL; i += 1) {
+    array[i] = i;
   }
 
-  return pElement;
+  return array;
+};
+
+const addCircleShipHits = ({ parent, ship }) => {
+  const shipName = createAnArrayOfNumbers({ shipName: ship });
+  const parentElement = document.querySelector(`${parent}`);
+  parentElement.classList.add('circles');
+
+  shipName.forEach((element) => {
+    const div = document.createElement('div');
+    div.setAttribute(`data-${ship}-hits`, `${element + 1}`);
+    div.classList.add('circle');
+    parentElement.appendChild(div);
+  });
+
+  return parentElement;
 };
 
 export default addCircleShipHits;
