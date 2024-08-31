9d017e2a2e2aa69c6fe228f75baa30dcae2a69a6
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-08-31 13:56:39 +0800

    feat: test out values to see if they hold strong references

--- src/test/player/player.test.js
+++ src/test/player/player.test.js
@@ -1,1 +1,13 @@
+/* eslint-disable no-undef */
 // test your public interface
+import { playersGameboard, computersGameboard } from './player';
+
+describe('checks for predertermined coordinates ships placement', () => {
+  test('should contain ships coordinates', () => {
+    expect(playersGameboard.shipPlaces.at(4).ship).toEqual('destroyer');
+  });
+
+  test('should contain enemy ships coordinates', () => {
+    expect(computersGameboard.shipPlaces.at(4).ship).toEqual('destroyer');
+  });
+});
