2d7ba45fb6c23473c89504a0bb6e7a13f751412e
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-09-02 08:51:46 +0800

    feat: Create a basicBattleship cpu that will be used against the player
    it's method setUnvisited() returns a random coordinate that will
    be used when the player missed hitting the ship in the enemy gameboard

--- src/test/player/player.js
+++ src/test/player/player.js
@@ -1,7 +1,57 @@
 import GAMEBOARD from '../gameboard/gameboard';
 
+const BasicBattleshipCPU = () => {
+  const visited = [];
+  const coordinates = [];
+
+  const getRandomNumber = ({ min, max }) =>
+    Math.floor(Math.random() * (max + 1 - min) + min);
+
+  const createGrid = () => {
+    const array = [];
+    let asciiOfA = 97; // -> 106 ---> a b c d e f g h i j;
+    let num = 1; // -> 10 ---> 1 2 3 4 5 6 7 8 9 10;
+
+    for (let i = 0; i < 100; i += 1) {
+      if (asciiOfA > 106) {
+        asciiOfA = 97;
+        num += 1;
+      }
+      array[i] = [num, String.fromCharCode(asciiOfA)];
+      asciiOfA += 1;
+    }
+
+    return array;
+  };
+
+  const setUnvisited = () => {
+    const randomNumber = getRandomNumber({ min: 0, max: 99 });
+    const [x, y] = createGrid()[randomNumber];
+
+    if (!visited.includes(`${x}-${y}`)) {
+      visited.push(`${x}-${y}`);
+      coordinates.push([x, y]);
+      return [x, y];
+    }
+
+    if (visited.length === 100) {
+      return [x, y];
+    }
+
+    return setUnvisited();
+  };
+
+  return {
+    visited,
+    coordinates,
+    getRandomNumber,
+    setUnvisited,
+  };
+};
+
+const computersGameboard = Object.assign(GAMEBOARD(), BasicBattleshipCPU());
+
 const playersGameboard = GAMEBOARD();
-const computersGameboard = GAMEBOARD();
 
 const ships = [
   // had to change it like this cuz it adds lines of codes
