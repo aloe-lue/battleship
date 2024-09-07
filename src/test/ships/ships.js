b9e1b0534191a3c78973d3a0011a275d3f5bf078
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-09-07 14:20:03 +0800

    feat: Capitalize methods following camelcase and uppercase constant prop

--- src/test/ships/ships.js
+++ src/test/ships/ships.js
@@ -1,32 +1,32 @@
 const Ships = () => {
-  const ships = {
-    carrier: { length: 5, hits: 0, sunk: false },
-    battleship: { length: 4, hits: 0, sunk: false },
-    submarine: { length: 3, hits: 0, sunk: false },
-    cruise: { length: 3, hits: 0, sunk: false },
-    destroyer: { length: 2, hits: 0, sunk: false },
+  const SHIPS = {
+    CARRIER: { LENGTH: 5, hits: 0, sunk: false },
+    BATTLESHIP: { LENGTH: 4, hits: 0, sunk: false },
+    SUBMARINE: { LENGTH: 3, hits: 0, sunk: false },
+    CRUISE: { LENGTH: 3, hits: 0, sunk: false },
+    DESTROYER: { LENGTH: 2, hits: 0, sunk: false },
   };
 
-  const hit = (ship) => {
-    ships[ship].hits += 1;
+  const Hit = ({ ship }) => {
+    SHIPS[ship].hits += 1;
   };
 
-  const isSunk = (ship) => {
-    ships[ship].sunk = ships[ship].length === ships[ship].hits;
+  const IsSunk = ({ ship }) => {
+    SHIPS[ship].sunk = SHIPS[ship].LENGTH === SHIPS[ship].hits;
   };
 
-  const getShipStatus = (ship) => {
-    const { hits } = ships[ship];
-    const { sunk } = ships[ship];
+  const GetShipStatus = ({ ship }) => {
+    const { hits } = SHIPS[ship];
+    const { sunk } = SHIPS[ship];
 
     return { hits, sunk };
   };
 
   return {
-    ships,
-    getShipStatus,
-    hit,
-    isSunk,
+    SHIPS,
+    GetShipStatus,
+    Hit,
+    IsSunk,
   };
 };
 
