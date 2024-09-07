5fcd4bccaf28742d621d97d8a564c8f903fdf56e
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-09-07 14:21:49 +0800

    fix: Follow the methods and properties letter casing

--- src/test/ships/ships.test.js
+++ src/test/ships/ships.test.js
@@ -5,40 +5,43 @@ const ship = Ships();
 /* eslint-disable no-undef */
 describe('hit function should increase the ship hits', () => {
   test('hit() should increases the ship hits', () => {
-    ship.hit('carrier');
+    ship.Hit({ ship: 'CARRIER' });
 
-    expect(ship.getShipStatus('carrier').hits).toEqual(1);
+    expect(ship.GetShipStatus({ ship: 'CARRIER' }).hits).toEqual(1);
   });
 
   test('hitting battleship twice should increase the ship hits to 2', () => {
-    ship.hit('battleship');
-    ship.hit('battleship');
+    ship.Hit({ ship: 'BATTLESHIP' });
+    ship.Hit({ ship: 'BATTLESHIP' });
 
-    expect(ship.getShipStatus('battleship').hits).toEqual(2);
+    expect(ship.GetShipStatus({ ship: 'BATTLESHIP' }).hits).toEqual(2);
   });
 
   test('sunk a ship', () => {
-    ship.hit('destroyer');
-    ship.hit('destroyer');
+    ship.Hit({ ship: 'DESTROYER' });
+    ship.Hit({ ship: 'DESTROYER' });
 
-    expect(ship.getShipStatus('destroyer').hits).toEqual(2);
+    expect(ship.GetShipStatus({ ship: 'DESTROYER' }).hits).toEqual(2);
   });
 });
 
 describe('test whether or not ship has been sunk', () => {
   test('destroyer is sunk', () => {
-    ship.isSunk('destroyer');
-    expect(ship.getShipStatus('destroyer').sunk).toBeTruthy();
+    ship.IsSunk({ ship: 'DESTROYER' });
+    expect(ship.GetShipStatus({ ship: 'DESTROYER' }).sunk).toBeTruthy();
   });
 
   test('battleship is not sunk yet', () => {
-    ship.isSunk('battleship');
-    expect(ship.getShipStatus('battleship').sunk).toBeFalsy();
+    ship.IsSunk({ ship: 'BATTLESHIP' });
+    expect(ship.GetShipStatus({ ship: 'BATTLESHIP' }).sunk).toBeFalsy();
   });
 });
 
 describe('test if you get back the values', () => {
   test('get destroyer status', () => {
-    expect(ship.getShipStatus('destroyer')).toEqual({ hits: 2, sunk: true });
+    expect(ship.GetShipStatus({ ship: 'DESTROYER' })).toEqual({
+      hits: 2,
+      sunk: true,
+    });
   });
 });
