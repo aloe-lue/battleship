f229a0d88f452c67023982adb8984224d960b536
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-09-08 06:12:05 +0800

    feat: Allow over the board coordinates but handle them when using it

--- src/test/gameboard/gameboard.js
+++ src/test/gameboard/gameboard.js
@@ -5,9 +5,8 @@ const HelperFunction = () => {
     const SHIPLEN = shipLength;
     const [x, y] = coordinates;
     const RESULT = [];
-    const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-    // findMatch
     let matchVal = 0;
     for (let i = 0; i < NUMBERS.length; i += 1) {
       const NUMBER = NUMBERS[i];
