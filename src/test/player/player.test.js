a06a02a72a8caf3140b9d2bdd4d0bb35cc9fb5ae
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-09-08 13:01:54 +0800

    fix: Follow the properties and methods casing
    feat: you want to make sure you get 5 ships

--- src/test/player/player.test.js
+++ src/test/player/player.test.js
@@ -1,27 +1,22 @@
 /* eslint-disable no-undef */
 // test your public interface
-import { playersGameboard, computersGameboard } from './player';
-
-describe('checks for predertermined coordinates ships placement', () => {
-  test('should contain ships coordinates', () => {
-    expect(playersGameboard.shipPlaces.at(4).ship).toEqual('destroyer');
-  });
-
-  test('should contain enemy ships coordinates', () => {
-    expect(computersGameboard.shipPlaces.at(4).ship).toEqual('destroyer');
-  });
-});
+import { computersGameboard, playersGameboard } from './player';
 
 test('visited length should be equal to how many set unvisited has been called', () => {
   for (let i = 0; i < 50; i += 1) {
-    computersGameboard.setUnvisited();
+    computersGameboard.SetUnvisited();
   }
 
-  expect(computersGameboard.visited.length).toEqual(50);
+  expect(computersGameboard.VISITED.length).toEqual(50);
 });
 
 test('computers set visited should reeturn until it returns unvisited coordinate', () => {
-  expect(computersGameboard.setUnvisited()).toEqual(
-    computersGameboard.coordinates.at(-1),
+  expect(computersGameboard.SetUnvisited()).toEqual(
+    computersGameboard.COORDINATES.at(-1),
   );
+});
+
+test('should contain ship places', () => {
+  expect(computersGameboard.SHIPPLACES.length).toBe(5);
+  expect(playersGameboard.SHIPPLACES.length).toBe(5);
 });
