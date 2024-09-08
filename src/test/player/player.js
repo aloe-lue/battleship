2af47f0a8f18439260ac049fb8f385f032da71e2
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-09-08 12:58:35 +0800

    fix: Uppercase properties and capitalize every words for method
    feat: you want to stop attacking the player's gameboard when cpu wins
    feat: you want to push randomized ship placement using the imported func

--- src/test/player/player.js
+++ src/test/player/player.js
@@ -1,173 +1,164 @@
-import GAMEBOARD from '../gameboard/gameboard';
+import Gameboard from '../gameboard/gameboard';
+import RandomShipsPlacement from '../generate_ship_coordinates/generate-ship-coordinates';
 
 // use the return value for adding a classList for an element
-const helperFunction = () => {
-  const isHit = ({ info }) => {
-    const infos = info;
-    return typeof infos === 'object';
+const HelperFunction = () => {
+  const IsHit = ({ info }) => {
+    const INFO = info;
+    return typeof INFO === 'object';
   };
 
-  const disableClick = ({ condition }) => {
-    if (condition) {
+  const DisableClick = ({ condition }) => {
+    const BOOL = condition;
+    if (BOOL) {
       return 'disable-pointer';
     }
 
     return 'enable-pointer';
   };
 
-  return { isHit, disableClick };
-};
-
-const BasicBattleshipCPU = () => {
-  const visited = [];
-  const coordinates = [];
-
-  const getRandomNumber = ({ min, max }) =>
+  const GetRandomNumber = ({ min, max }) =>
     Math.floor(Math.random() * (max + 1 - min) + min);
 
-  const createGrid = () => {
-    const array = [];
-    let asciiOfA = 97; // -> 106 ---> a b c d e f g h i j;
-    let num = 1; // -> 10 ---> 1 2 3 4 5 6 7 8 9 10;
+  const CreateGrid = () => {
+    const ARRAY = [];
+    let asciiOfA = 97;
+    let num = 1;
 
     for (let i = 0; i < 100; i += 1) {
       if (asciiOfA > 106) {
         asciiOfA = 97;
         num += 1;
       }
-      array[i] = [num, String.fromCharCode(asciiOfA)];
+      ARRAY[i] = [num, String.fromCharCode(asciiOfA)];
       asciiOfA += 1;
     }
 
-    return array;
+    return ARRAY;
   };
+
+  const GetRandomShipPlacement = () => {
+    const AXE = ['vertical', 'horizontal'];
+    const SHIPS = ['CARRIER', 'BATTLESHIP', 'SUBMARINE', 'CRUISE', 'DESTROYER'];
+
+    const RANDOMNUMBER = GetRandomNumber({ min: 0, max: 99 });
+    const AXISRAND = GetRandomNumber({ min: 0, max: 1 });
+    const SHIPRAND = GetRandomNumber({ min: 0, max: 4 });
+
+    const COORDINATE = CreateGrid()[RANDOMNUMBER];
+    const AXIS = AXE[AXISRAND];
+    const SHIP = SHIPS[SHIPRAND];
+
+    return { COORDINATE, AXIS, SHIP };
+  };
+
+  return {
+    IsHit,
+    DisableClick,
+    GetRandomNumber,
+    CreateGrid,
+    GetRandomShipPlacement,
+  };
+};
+
+const BasicBattleshipCPU = () => {
+  const VISITED = [];
+  const COORDINATES = [];
 
   // coordinate randomizer
-  const setUnvisited = () => {
-    const randomNumber = getRandomNumber({ min: 0, max: 99 });
-    const [x, y] = createGrid()[randomNumber];
+  const SetUnvisited = () => {
+    const { GetRandomNumber, CreateGrid } = HelperFunction();
+    const RANDOMNUMBER = GetRandomNumber({ min: 0, max: 99 });
+    const [x, y] = CreateGrid()[RANDOMNUMBER];
 
-    if (!visited.includes(`${x}-${y}`)) {
-      visited.push(`${x}-${y}`);
-      coordinates.push([x, y]);
+    if (!VISITED.includes(`${x}-${y}`)) {
+      VISITED.push(`${x}-${y}`);
+      COORDINATES.push([x, y]);
       return [x, y];
     }
 
-    if (visited.length === 100) {
+    if (VISITED.length === 100) {
       return [x, y];
     }
 
-    return setUnvisited();
+    return SetUnvisited();
   };
 
-  const attackPlayerSquare = ({ hitInfo, status, gameboard, array = [] }) => {
-    const isMissed = hitInfo;
-    const stat = status;
+  const AttackPlayerSquare = ({
+    hitInfo,
+    status,
+    player,
+    gameboard,
+    array = [],
+  }) => {
+    const ISMISSED = hitInfo;
+    const STAT = status;
 
-    if (isMissed === false) {
-      return array;
-    }
-    if (stat === true) {
-      return array;
-    }
-
-    const coordinate = setUnvisited();
-    const attackInfo = gameboard.receiveAttack({
-      coordinate,
+    const ISPLAYERSHIPSSUNK = player.IsShipAllSunk({
+      enemyGameboard: gameboard.SHIPS,
     });
-    const { isHit } = helperFunction();
-    const isShipMissed = isHit({ info: attackInfo });
+
+    if (ISPLAYERSHIPSSUNK) {
+      return array;
+    }
+
+    if (ISMISSED === false) {
+      return array;
+    }
+    if (STAT === true) {
+      return array;
+    }
+
+    const COORDINATE = SetUnvisited();
+    const ATTACKINFO = gameboard.ReceiveAttack({
+      coordinate: COORDINATE,
+    });
+
+    const { IsHit } = HelperFunction();
+    const ISSHIPMISSED = IsHit({ info: ATTACKINFO });
 
     return array.concat(
-      { coordinate, isShipMissed },
-      attackPlayerSquare({ hitInfo, gameboard, status: isShipMissed, array }),
+      { COORDINATE, ISSHIPMISSED },
+      AttackPlayerSquare({
+        hitInfo,
+        gameboard,
+        status: ISSHIPMISSED,
+        array,
+        player,
+      }),
     );
   };
 
   return {
-    visited,
-    coordinates,
-    getRandomNumber,
-    setUnvisited,
-    attackPlayerSquare,
+    VISITED,
+    COORDINATES,
+    SetUnvisited,
+    AttackPlayerSquare,
   };
 };
 
-const computersGameboard = Object.assign(GAMEBOARD(), BasicBattleshipCPU());
+const computersGameboard = Object.assign(Gameboard(), BasicBattleshipCPU());
 
-const playersGameboard = GAMEBOARD();
+const playersGameboard = Gameboard();
 
-const ships = [
-  {
-    ship: 'carrier',
-    coordinates: [2, 'b'],
-    axe: 'h',
-  },
-  {
-    ship: 'battleship',
-    coordinates: [4, 'b'],
-    axe: 'h',
-  },
-  {
-    ship: 'submarine',
-    coordinates: [3, 'h'],
-    axe: 'h',
-  },
-  {
-    ship: 'cruise',
-    coordinates: [7, 'a'],
-    axe: 'h',
-  },
-  {
-    ship: 'destroyer',
-    coordinates: [9, 'f'],
-    axe: 'v',
-  },
-];
+RandomShipsPlacement()
+  .SetShipsPlaces({
+    places: [],
+    placed: new Set(),
+    helperFunction: HelperFunction,
+  })
+  .forEach((coordinate) => {
+    computersGameboard.SHIPPLACES.push(coordinate);
+  });
 
-ships.map((element) =>
-  playersGameboard.getShipCoordinates({
-    pairOfCoordinates: element.coordinates,
-    ship: element.ship,
-    axis: element.axe,
-  }),
-);
+RandomShipsPlacement()
+  .SetShipsPlaces({
+    places: [],
+    placed: new Set(),
+    helperFunction: HelperFunction,
+  })
+  .forEach((coordinate) => {
+    playersGameboard.SHIPPLACES.push(coordinate);
+  });
 
-const enemyShips = [
-  // had to change it like this cuz it adds lines of codes
-  {
-    ship: 'carrier',
-    coordinates: [3, 'i'],
-    axe: 'v',
-  },
-  {
-    ship: 'battleship',
-    coordinates: [3, 'c'],
-    axe: 'v',
-  },
-  {
-    ship: 'submarine',
-    coordinates: [1, 'f'],
-    axe: 'v',
-  },
-  {
-    ship: 'cruise',
-    coordinates: [8, 'd'],
-    axe: 'h',
-  },
-  {
-    ship: 'destroyer',
-    coordinates: [1, 'a'],
-    axe: 'v',
-  },
-];
-
-enemyShips.map((element) =>
-  computersGameboard.getShipCoordinates({
-    pairOfCoordinates: element.coordinates,
-    ship: element.ship,
-    axis: element.axe,
-  }),
-);
-
-export { playersGameboard, computersGameboard, helperFunction };
+export { playersGameboard, computersGameboard, HelperFunction };
