9664f6ba8cbdbfe15d682edcf0bd5acb67217f79
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-09-09 18:31:21 +0800

    feat: Create a pop up that appears when there's a winner

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -6,10 +6,13 @@ const TopNotification = ({ parent }) => {
 
   button.innerText = 'Restart';
 
+  h2.classList.add('winner');
+  button.classList.add('restart');
   div.classList.add('announce-winner');
   div.appendChild(h2);
   div.appendChild(button);
   parentEl.appendChild(div);
+
   return { div, h2, button, parentEl };
 };
 
