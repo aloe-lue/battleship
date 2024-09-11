2ae1d50dda0531a35417925e3d31320bea392da8
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-09-11 17:21:11 +0800

    fix: remove favicon plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -20,7 +19,6 @@ module.exports = {
       filename: '[name].css',
       chunkFilename: '[id].css',
     }),
-    new FaviconsWebpackPlugin('src/img/battleship-logo.ico'),
   ],
   devServer: {
     static: './src',
