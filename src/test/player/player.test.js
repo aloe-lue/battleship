7e0f74e0bb00f39aff72e34b2c8e216564be759d
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-09-14 13:43:09 +0800

    feat: test get adjacent slot that returns array containing adjacent item

--- src/test/player/player.test.js
+++ src/test/player/player.test.js
@@ -1,22 +1,27 @@
 /* eslint-disable no-undef */
 // test your public interface
-import { computersGameboard, playersGameboard } from './player';
+import { ComputerGameboard } from './player';
+
+const { computersGameboard } = ComputerGameboard();
 
 test('visited length should be equal to how many set unvisited has been called', () => {
   for (let i = 0; i < 50; i += 1) {
     computersGameboard.SetUnvisited();
   }
-
-  expect(computersGameboard.VISITED.length).toEqual(50);
+  expect(computersGameboard.VISITED.size).toEqual(50);
 });
 
-test('computers set visited should reeturn until it returns unvisited coordinate', () => {
-  expect(computersGameboard.SetUnvisited()).toEqual(
-    computersGameboard.COORDINATES.at(-1),
-  );
-});
+describe('say you hit a ship you are given a chance to hit again', () => {
+  test('get adjacent slot of hit coordinate, use it to guess where to hit', () => {
+    expect(
+      computersGameboard.HitAdjacent({ coordinate: [2, 'b'], ship: 'carrier' }),
+    ).toEqual([
+      [1, 'b'],
+      [2, 'a'],
+      [3, 'b'],
+      [2, 'c'],
+    ]);
+  });
 
-test('should contain ship places', () => {
-  expect(computersGameboard.SHIPPLACES.length).toBe(5);
-  expect(playersGameboard.SHIPPLACES.length).toBe(5);
+  test("shouldn't hit the hit", () => {});
 });
